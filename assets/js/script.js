84a6d0c2055737d67cf05035b5b98545bff3baf9
Thomas Ciszek <tciszek515@gmail.com>
2022-03-17 16:11:17 -0400

    refactored code for better readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,40 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskEl = function(taskDataObj) {
 
-    event.preventDefault();
-
-    var taskNameInput = document.querySelector("input[name='task-name']").value;
-    var taskTypeInput = document.querySelector("select[name='task-type']").value;
-
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
+    // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+}
+
+var taskFormHandler = function() {
+
+    // stops page from refreshing when submitted
+    event.preventDefault();
+
+    // pulls the user inputted name and type of tasks
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
